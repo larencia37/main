31a3ee0cb5a75b00345744b3d184a8dbf55e3a74
larencia37 <162613801+larencia37@users.noreply.github.com>
2026-06-24 06:26:06 +0000

    .

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str = 'postgresql+psycopg2://postgres:postgres@db:5432/venehealth'
